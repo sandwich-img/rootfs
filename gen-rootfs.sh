576ae303e42b5ed0c193d58b311e481733175f88
yangxuan8282 <yangxuan8282@gmail.com>
2019-01-17 18:02:22 +0800

    archlinux: update gen-rootfs.sh

--- gen-rootfs.sh
+++ gen-rootfs.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-mkdir -p $ARCH
+mkdir -p $ARCH/rootfs
 
-pacstrap $ARCH base base-devel &&
+pacstrap $ARCH/rootfs base base-devel &&
 tar --numeric-owner --create --auto-compress --file $ARCH/rootfs.tar.xz --directory $ARCH/rootfs --transform='s,^./,,' . &&
-rm -rf $ALPINE_BRANCH/$ARCH/rootfs
+rm -rf $ARCH/rootfs
